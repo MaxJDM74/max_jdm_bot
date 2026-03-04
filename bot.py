38dbe12db90d6c7cc96fb298c01df824991e7056
MaxJDM74 <verisa050@gmail.com>
2026-03-04 23:00:18 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ import logging
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
-# Настройка логирования
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 TOKEN = os.environ.get('TELEGRAM_TOKEN')
@@ -19,6 +18,7 @@ def main():
         print("❌ Нет токена!")
         return
     
+    print("✅ Запускаю бота...")
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler('start', start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
